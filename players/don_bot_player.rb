c614f0c3a242e32029a3d2e1169e9d9d9f2af00e
Don Pflaster <dpflaster@gmail.com>
2014-02-15 14:23:49 -0500

    Don Bot completed with logging

--- players/don_bot_player.rb
+++ players/don_bot_player.rb
@@ -23,10 +23,12 @@ class DonBotPlayer
     @potentials = get_potentials
     @ships_remaining = ships_remaining
     if @potentials.any?
+      #@log << @potentials.map { |p| [p,p[:type]]}.inspect + "\n"
       vectors   = @potentials.select { |p| p[:type] == :vector }
       lone_hits = @potentials.select { |p| p[:type] == :lone_hit }
       if vectors.any?
-        smart_targets = vectors.select { |v| follow_vector(v) }
+        smart_targets = vectors.map { |v| follow_vector(v[:adjacent],v[:from_hit]) }.reject(&:nil?)
+        @log << "SMART TARGETS: " + smart_targets.inspect + "\n"
         if smart_targets.any?
           smart_targets.first
         elsif lone_hits.any?
@@ -61,6 +63,7 @@ class DonBotPlayer
   end
 
   def fire_at_random(collection)
+    @log << "FIRING AT " + collection.inspect +  "\n"
     collection[rand(collection.size)]
   end
 
@@ -81,13 +84,14 @@ class DonBotPlayer
       all_adjacents = adjacents(hit[0],hit[1])
       hits       = adjacents_by_type(all_adjacents,:hit)
       unknowns   = adjacents_by_type(all_adjacents,:unknown)
+      #misses     = adjacents_by_type(all_adjacents,:miss)
       vector_found = false
       unknowns.each { |adjacent|
         if hits.include?(reciprocal_adjacent(hit,adjacent))
           type = :vector
           vector_found = true
         end
-        this_hit_potentials << { :adjacent => adjacent, :type => type }
+        this_hit_potentials << { :adjacent => adjacent, :type => type, :from_hit => hit }
       }
       if vector_found == false
         this_hit_potentials.each { |thp| thp[:type] = :lone_hit }
@@ -102,20 +106,98 @@ class DonBotPlayer
   end
 
   def adjacents(y,x)
-    [[y-1,x],[y+1,x],[y,x+1],[y,x-1]].reject { |a| a[1] < 0 || a[0] < 0 || a[1] >= @board_size || a[0] >= @board_size }
+    [[y-1,x],[y+1,x],[y,x+1],[y,x-1]].reject { |a| !in_bounds(a) }
   end
 
-  def follow_vector(vector)
+  def follow_vector(vector,from_hit)
+    adjacent_hits = adjacents_by_type(adjacents(vector[0],vector[1]),:hit)
+    largest_remaining_ship = @ships_remaining.sort.last
+    @log << "LARGEST REMAINING: "
+    @log << largest_remaining_ship.to_s + "\n"
+    directions = get_vector_directions(adjacent_hits,vector)
+    @log << "DIRECTIONS: "
+    @log << directions.inspect + "\n"
+    unknowns = grab_unknowns_from_ends(directions,from_hit) || []
+    @log << "UNKNOWNS GRABBED: " + unknowns.inspect + "\n"
+    unknowns if unknowns.any?
+  end
+
+  def get_vector_directions(adjacent_hits,vector)
+    adjacent_hits.map { |hit| (hit[0] == vector[0] ? :column : :row) }
   end
 
   def reciprocal_adjacent(hit,adjacent)
     # Return the adjacent on the opposite side of the hit
     if adjacent[0] != hit[0]
-      ra = [ (adjacent[0] > hit[0] ? hit[0] - 1 : hit[0] + 1), adjacent[1] ]
+      [ (adjacent[0] > hit[0] ? hit[0] - 1 : hit[0] + 1), adjacent[1] ]
     else
-      ra = [ adjacent[0], (adjacent[1] > hit[1] ? hit[1] - 1 : hit[1] + 1) ]
+      [ adjacent[0], (adjacent[1] > hit[1] ? hit[1] - 1 : hit[1] + 1) ]
     end
-    ra
+  end
+
+  def grab_unknowns_from_ends(directions,vector)
+    # Unless it isn't worth it
+    candidates = []
+    phase_results = []
+    directions.each { |direction|
+      if direction == :column
+        phase_results = [:up,:down].map { |phase| pointer_phase(phase,vector) }
+      else
+        phase_results = [:left,:right].map { |phase| pointer_phase(phase,vector) }
+      end
+    }
+    @log << "PHASE RESULTS COMPLETE. "
+    @log << phase_results.inspect + "\n"
+    hits_length = phase_results.map { |pr| pr[0] }.inject(:+) - 1
+    @log << "HITS LENGTH " + hits_length.to_s + "\n"
+    if hits_length <= @ships_remaining.sort.last
+      # This ship might be bigger than the largest outstanding ship.  Consider the unknowns as candidates
+      phase_results.reject{ |pr| pr[1].nil? }.map { |pr| pr[1] }[0]
+    end
+  end
+
+  def pointer_phase(phase,vector)
+    @log << "PHASE " + phase.to_s + "\n"
+    pointer = vector.clone
+    #@log << "POINTER: " + pointer.inspect + "\n"
+    hit_count = 0
+    #pointer = increment_pointer(pointer,phase)
+    #candidate = pointer.clone if board_state(pointer) == :unknown
+    #@log << "POINTER NOW AT " + pointer.inspect + "\n"
+    #@log << "BOARD HAS: " + board_state(pointer).inspect + "\n"
+
+    previous_pointer = pointer.clone
+    while in_bounds(pointer) && board_state(previous_pointer) == :hit
+      previous_pointer = pointer.clone
+      new_state = board_state(pointer)
+      @log << "POINTER NOW AT " + pointer.inspect + "\n"
+      @log << "BOARD HAS: " + new_state.inspect + "\n"
+      if new_state == :unknown
+        candidate = pointer.clone
+      else
+        hit_count += 1
+      end
+      @log << "Number of hits catalogued: " + hit_count.to_s + "\n"
+      pointer = increment_pointer(pointer,phase)
+    end
+    [hit_count,candidate]
+  end
+
+  def increment_pointer(pointer,phase)
+    pointer[0] += 1 if phase == :right
+    pointer[0] -= 1 if phase == :left
+    pointer[1] += 1 if phase == :down
+    pointer[1] -= 1 if phase == :up
+    pointer
+  end
+
+  def in_bounds(coords)
+    coords[0] >= 0 && coords[1] >= 0 &&
+    coords[0] < @board_size && coords[1] < @board_size
+  end
+
+  def board_state(pointer)
+    @state[pointer[1]][pointer[0]]
   end
 end
 
